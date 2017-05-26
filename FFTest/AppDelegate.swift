de408dfbbe6323bef53a56ad365b198ff335a513
Francisco Amado <franciscoamado@live.com.pt>
2017-05-26 02:19:15 +0100

    Init Project
    min version iOS 9.0
    added WMO to debug builds
    removes Main.storyboard

---
+++ FFTest/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  FFTest
+//
+//  Created by Francisco Amado on 26/05/2017.
+//  Copyright © 2017 franciscoamado. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        
+        return true
+    }
+}
---
+++ FFTest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FFTest
+//
+//  Created by Francisco Amado on 26/05/2017.
+//  Copyright © 2017 franciscoamado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.darkGray
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
